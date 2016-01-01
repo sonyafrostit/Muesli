12a0f633d6f7738f75c59e2dafa7e86247ea91b0
bmsmith <sengbms@yahoo.com>
2016-01-01 15:39:18 +0000

    Swapped tabs for spaces, added in code to read in json file with api data (secret + key) for lastfm.

--- muesli.js
+++ muesli.js
@@ -1,8 +1,21 @@
 var http = require("http");
 var request = require("request");
+var fs = require("fs");
+
+var apiData;
+
+function loadData() {
+    fs.readFile( __dirname + "apidata.json", function (err, data) {
+        if (err) {
+            throw err;
+        } 
+        apiData = JSON.parse(data);
+    });
+}
+
 
 http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Welcome to Muesli!");
-	response.end();
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Welcome to Muesli!");
+    response.end();
 }).listen(8080);
